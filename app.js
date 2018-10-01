a819650adb636a49e800d6d9adc4caedbf5237d2
shalvah <diakon.ng@gmail.com>
2018-10-01 21:46:15 +0100

    Update auth system

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 const passport = require('passport');
-const Strategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new Strategy((username, password, done) => {
+passport.use(new LocalStrategy((username, password, done) => {
         if (username === 'admin' && password === 'secret') {
             return done(null, {username});
         }
         return done(null, null)
     })
 );
-
-passport.serializeUser(function (user, cb) {
-    cb(null, user.username);
-});
-
-passport.deserializeUser(function (username, cb) {
-    cb(null, {username});
-});
+passport.serializeUser((user, cb) => cb(null, user.username));
+passport.deserializeUser((username, cb) => cb(null, { username }));
 
 const app = express();
 
@@ -30,8 +24,8 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'anything' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
