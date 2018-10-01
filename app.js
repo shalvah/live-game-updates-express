f20b031f0605807bd2dfc273f52e0c9ba2a08f33
shalvah <diakon.ng@gmail.com>
2018-10-01 19:49:46 +0100

    Set up auth

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+passport.use(new Strategy((username, password, done) => {
+        if (username === 'admin' && password === 'secret') {
+            return done(null, {username});
+        }
+        return done(null, null)
+    })
+);
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.username);
+});
+
+passport.deserializeUser(function (username, cb) {
+    cb(null, {username});
+});
 
 const app = express();
 
@@ -15,27 +29,12 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/', require('./routes/index'));
 
 module.exports = app;
